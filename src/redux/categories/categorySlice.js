274d87f8dccd41544613a7093ce886f181a9409e
Ikechukwu <bonifacedennis193@gmail.com>
2023-05-10 14:49:32 +0100

    reducer added to project

---
+++ src/redux/categories/categorySlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  categories: [],
+  total: 0,
+  isLoading: true,
+};
+
+const categorySlice = createSlice({
+  name: 'book',
+  initialState,
+  reducers: {
+    bookStatus: (state) => {
+      state.books = [];
+    }
+  },
+});
+
+export const { bookStatus } = categorySlice.actions;
+
+export default categorySlice.reducer;
