794eaef54dcb5ddafda29055deb11674c3bd2b6a
Ikechukwu <bonifacedennis193@gmail.com>
2023-05-11 09:37:28 +0100

    optional correction made

--- src/redux/books/bookSlice.js
+++ src/redux/books/bookSlice.js
@@ -11,9 +11,7 @@ const bookSlice = createSlice({
   initialState,
   reducers: {
     addBook: (state, action) => {
-      state.books.push(
-        action.payload,
-      );
+      state.books = [...state.books, action.payload];
     },
     removeBook: (state, action) => {
       const itemId = action.payload;
