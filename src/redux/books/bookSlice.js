c717fc3f7358a24c55afda2cdd6bfeda31fc855f
Ikechukwu <bonifacedennis193@gmail.com>
2023-05-12 18:50:49 +0100

    code reviever issues resolved

--- src/redux/books/bookSlice.js
+++ src/redux/books/bookSlice.js
@@ -44,7 +44,6 @@ const bookSlice = createSlice({
       state.books = [...state.books, action.payload];
     },
     removeBook: (state, action) => {
-      console.log(state);
       const itemId = action.payload;
       state.books = state.books.filter((item) => item.item_id !== itemId);
     },
