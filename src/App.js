788cae59ad8e68ca70ebeaf7ed1bc29e629b799d
Ikechukwu <bonifacedennis193@gmail.com>
2023-08-21 15:42:38 +0100

    updated directory

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { BrowserRouter as CreateElement, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Categories from './components/Categories';
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,3 @@
-
 import { configureStore } from '@reduxjs/toolkit';
 import bookSlice from './books/bookSlice';
 import categorySlice from './categories/categorySlice';
